5fdd75f1c5512fc438bc560398809f27d927f7d6
Ben Zech <72528903+TechZech@users.noreply.github.com>
2021-09-13 18:47:56 -0400

    Assignment part 2
    part 2 of the project

---
+++ MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Benjamin Zech on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
